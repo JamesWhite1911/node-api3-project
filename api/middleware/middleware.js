fdf8383a065259f3239f15a4fda5f287c936e1fd
James <JamesWhite1911@protonmail.com>
2021-03-10 18:54:34 -0500

    middleware finished

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,29 +1,75 @@
+const Users = require('../users/users-model')
+
 function logger(req, res, next) {
   // logger logs to the console the following information about each request: request method, request url, and a timestamp
   // this middleware runs on every request made to the API
+  res.use('*', (req, res, next) => {
+    console.log(req.method, req.url, Date.now())
+    next()
+  })
 }
 
-function validateUserId(req, res, next) {
-  // this middleware will be used for all user endpoints that include an id parameter in the url
-  // (ex: /api/users/:id and it should check the database to make sure there is a user with that id.
-  // if the id parameter is valid, store the user object as req.user and allow the request to continue
-  // if the id parameter does not match any user id in the database, respond with status 404 and { message: "user not found" }
+const validateUserId = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const user = await User.getById(id)
+  
+    const alert = res.status(404)
+    const notFound = { message: "user not found" }
+
+    if (!user) {
+      alert.json(notFound)
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 function validateUser(req, res, next) {
-  // validateUser validates the body on a request to create or update a user
-  // if the request body is missing, respond with status 400 and { message: "missing user data" }
-  // if the request body lacks the required name field, respond with status 400 and { message: "missing required name field" }
+  const { body } = req
+
+  const alert = res.status(400)
+  const missing = { message: "missing user data" } //potential issue with const
+
+  if (!body) {
+    alert.json(missing)
+    if (!body.name) {
+      missing = { message: "missing required name field" }
+      alert.json(missing)
+    }
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // validatePost validates the body on a request to create a new post
-  // if the request body is missing, respond with status 400 and { message: "missing post data" }
-  // if the request body lacks the required text or user_id fields, respond with status 400 and { message: "missing required text or user_id fields" }
+  const { body } = req
+
+  const alert = res.status(400)
+  const missing = { message: "missing post data" } //potential issue with const
+
+  if (!body) {
+    alert.json(missing)
+    missing = { message: "missing required text or user_id fields" }
+    if (!body.text || !body.user_id) {
+      alert.json(missing)
+    }
+  } else {
+    next()
+  }
+}
+
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
 }
 
 // do not forget to expose these functions to other modules
-
 
 
 //POSTS
